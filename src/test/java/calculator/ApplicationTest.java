b2654b00c2c2a041ccbce6aefbcad4708e932893
nonaninona <starcraft0529@gmail.com>
2024-10-18 18:48:18 +0900

    test(ApplicationText.java) : add test for various cases
    기본 구분자 사용 시
    - 숫자 없음
    - 숫자 1개
    - 여러 구분자 섞어서 사용
    커스텀 구분 자 사용 시
    - 숫자 없음
    - 숫자 1개
    - 긴 커스텀 구분자
    - 여러 구분자 섞어서 사용
    예외 테스트
    - 음수가 포함된 입력
    - 음수 1개 입력
    - 문자 입력
    - 너무 큰 숫자 입력

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 기본_구분자_사용_숫자_없음() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+    @Test
+    void 기본_구분자_사용_숫자_1개() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용_구분자_섞어서_사용() {
+        assertSimpleTest(() -> {
+            run("1,3:4,6");
+            assertThat(output()).contains("결과 : 14");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_숫자_없음() {
+        assertSimpleTest(() -> {
+            run("//;\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -17,10 +47,49 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용_긴커스텀구분자() {
+        assertSimpleTest(() -> {
+            run("//;;;\\n1;;;3");
+            assertThat(output()).contains("결과 : 4");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_구분자_섞어서_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;3,5:5;3");
+            assertThat(output()).contains("결과 : 16");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트_음수1개() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("-3"))
+                    .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("abcdef"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_너무큰숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("9999999999999999999,9999999999999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
