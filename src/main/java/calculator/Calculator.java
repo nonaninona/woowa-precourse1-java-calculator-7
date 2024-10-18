8ddba133426a31a1caa4802701f8dcaf46efe340
nonaninona <starcraft0529@gmail.com>
2024-10-18 13:39:09 +0900

    style(Calculator.java) : remove unnecessary comment

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,8 +5,6 @@ public class Calculator {
     private Adder adder = new Adder();
 
     public int add() {
-        // 캡슐화 굿, 응집도 애매... view랑 비즈니스 로직이 같이 있다, 결합도도 애매... 3개나 의존함
-        // Adder로 비즈니스 로직 분리
         return adder.add(userInterface.getNumbers());
     }
 }
