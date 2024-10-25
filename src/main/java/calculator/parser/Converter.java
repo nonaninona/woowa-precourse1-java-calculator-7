6a8c92b277fdb8ace8f5041078faffe663bb5dfd
nonaninona <starcraft0529@gmail.com>
2024-10-25 15:39:57 +0900

    feat(Converter.java) : add Exception condition
    음수, 문자가 포함된 수, 너무 큰 수에 대한 exception 추가

--- src/main/java/calculator/parser/Converter.java
+++ src/main/java/calculator/parser/Converter.java
@@ -4,15 +4,15 @@ import calculator.MyNumber;
 
 public class Converter {
     public static MyNumber[] convertNumbers(String[] numbers) {
-        for (String number : numbers) {
-            if (number.contains("-")) {
-                throw new IllegalArgumentException("잘못된 입력 : 음수");
-            }
-        }
-
         MyNumber[] myNumbers = new MyNumber[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            myNumbers[i] = new MyNumber(numbers[i]);
+        try {
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i].contains("-"))
+                    throw new IllegalArgumentException("잘못된 입력 : 음수");
+                myNumbers[i] = new MyNumber(numbers[i]);
+            }
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력 : 너무 큰 수 또는 문자가 포함된 수");
         }
         return myNumbers;
     }
