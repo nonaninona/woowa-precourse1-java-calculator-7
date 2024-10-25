8adc1381770bb578dc6bfcd55ddb4ba8abb1e6a4
nonaninona <starcraft0529@gmail.com>
2024-10-25 14:01:30 +0900

    fix(DefaultSplitter.java) : add missing Override annotation

--- src/main/java/calculator/parser/splitter/DefaultSplitter.java
+++ src/main/java/calculator/parser/splitter/DefaultSplitter.java
@@ -3,6 +3,7 @@ package calculator.parser.splitter;
 import calculator.MyRegex;
 
 public class DefaultSplitter implements Splitter {
+    @Override
     public String[] splitNumbers(String input) {
         MyRegex regex = new MyRegex();
         return input.split(regex.getRegex());
